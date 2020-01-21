94edfc139b1c4917fa4fdc682d1be37c4578dfe9
Rhea Kapur <rhea.kapur1@gmail.com>
2020-01-21 15:25:36 -0500

    good version of prelim_analysis

--- prelim_analysis.R
+++ prelim_analysis.R
@@ -2,12 +2,12 @@ require(ggplot2)
 require(tidyverse)
 
 require("mongolite")
-connection <- mongo(collection = "Experiments", db = "ShallowMind", url = "mongodb://localhost")
-mongo
+# 
+m <- mongo(collection = "Experiments", db = "ShallowMind", url = "mongodb://rkapur2021:okay4kench635shallowmind.pingry.org:27017")
 
-datasets <- read.csv(file="/Users/RheaMacBook/Desktop/shallowmind_csvs/datasets.csv")
+# datasets <- read.csv(file="/Users/RheaMacBook/Desktop/shallowmind_csvs/datasets.csv")
 experiments <- read.csv(file="/Users/RheaMacBook/Desktop/shallowmind_csvs/experiments.csv")
-neural_nets <- read.csv(file="/Users/RheaMacBook/Desktop/shallowmind_csvs/neuralNets.csv")
+# neural_nets <- read.csv(file="/Users/RheaMacBook/Desktop/shallowmind_csvs/neuralNets.csv")
 
 # example. structure [1,1,1] so cubic
 
@@ -23,19 +23,82 @@ convert <- function(x) {
   abs(training_over_time)
 }
 
-experiments$trainingAccuracyOverTime <- lapply(experiments$trainingAccuracyOverTime, convert)
-experiments$validationAccuracyOverTime <- lapply(experiments$validationAccuracyOverTime, convert)
+experiments$trainingAccuracyOverTime <- lapply(experiments$trainingLossOverTime, convert)
+experiments$validationAccuracyOverTime <- lapply(experiments$validationLossOverTime, convert)
 # PROBLEM WITH THIS ONE
 experiments$neuralNetHiddenStructure <- lapply(experiments$neuralNetHiddenStructure, convert)
 experiments$inputShape <- as.numeric(experiments$inputShape)
 experiments$outputShape <- as.numeric(experiments$outputShape)
 
-mean_diff_over_time <- function(vector){
-    diffs <- vector[-1] - head(vector, -1)
-    mean(diffs)
+row <- 1110
+training <- data.frame(epoch = seq(1,length(experiments$trainingAccuracyOverTime[[row]]), 1), trainAcc = experiments$trainingAccuracyOverTime[[row]]) 
+validation <- data.frame(epoch = seq(1,length(experiments$validationAccuracyOverTime[[row]]), 1), valAcc = experiments$validationAccuracyOverTime[[row]]) 
+graph <- ggplot() + 
+  geom_line(data = training, aes(x = epoch, y = trainAcc), color = "blue") +
+  geom_line(data = validation, aes(x = epoch, y = valAcc), color = "red") +
+  labs(title=, x="Epoch", y="Accuracy") # Adding scatterplot geom (layer1) and smoothing geom (layer2).
+graph
+
+epochToAccuracy <- function(epoch, shape) {
+  rowIndex <- Position(function(x) identical(x, shape), experiments$neuralNetHiddenStructure)
+  accuracyList <- experiments$trainingAccuracyOverTime[[rowIndex]]
+  if (epoch > 0 & epoch <= length(accuracyList)) { 
+    return(accuracyList[epoch])
+  }
+  else { 
+    return(-1)
+  }
+}
+
+mean_connectivity <- function(struct) {
+  copy <- sapply(struct, function(i) i)
+  struct <- struct[-1]
+  copy <- copy[-length(copy)]
+  a <- mean(copy*struct)
+  return(a)
+}
+
+mean_diff_over_time <- function(vector) {
+  vector <- unlist(vector)
+  diffs <- vector[-1] - head(vector, -1)
+  mean(diffs)
 }
 
 std_diff_over_time <- function(vector){
-    diffs <- vector[-1] - head(vector, -1)
-    sd(diffs)
+  vector <- unlist(vector)
+  diffs <- vector[-1] - head(vector, -1)
+  sd(diffs)
 }
+
+experiments$avg_training_rate <- lapply(experiments$trainingAccuracyOverTime, mean_diff_over_time)
+experiments$training_volatility <- lapply(experiments$trainingAccuracyOverTime, std_diff_over_time)
+
+# Coefficient of overfitting for an entire neural net 
+# Returns list of 2 elements: first, coefficient of overfitting for neural net over the course of the epochs, and then, average coefficient of overfitting (so average over the epochs)
+coeff_overfit_nn <- function(shape) {
+  rowIndex <- Position(function(x) identical(x, shape), experiments$neuralNetHiddenStructure)
+  
+  training_accuracy_per_row <- experiments$trainingAccuracyOverTime[[rowIndex]]
+  validation_accuracy_per_row <- experiments$validationAccuracyOverTime[[rowIndex]]
+  coeff_over_epochs <- training_accuracy_per_row - validation_accuracy_per_row
+  return(list(coeff_over_epochs, mean(coeff_over_epochs)))
+}
+
+coeff_overfit_epoch <- function(epoch, shape) {
+  rowIndex <- Position(function(x) identical(x, shape), experiments$neuralNetHiddenStructure)
+  training_accuracy_per_row <- experiments$trainingAccuracyOverTime[[rowIndex]]
+  validation_accuracy_per_row <- experiments$validationAccuracyOverTime[[rowIndex]]
+  overfit = training_accuracy_per_row - validation_accuracy_per_row
+  abs(overfit)
+}
+
+# coeff_overfit_nn(c(1,0))
+experiments$overfitting <- lapply(experiments$neuralNetHiddenStructure, coeff_overfit_nn)
+
+# graph overfitting vs. complexity of neural net (increasing structures)
+
+# mean training rate vs. connectivity
+# plot different functions vs. connectivity
+
+
+
