8f0589174dd3714cafde21ecdd07e4d3c8de7cbf
EmmetH11 <57051119+EmmetH11@users.noreply.github.com>
2020-02-05 18:29:51 -0500

    Update dataReviewGraphs.R

--- dataReviewGraphs.R
+++ dataReviewGraphs.R
@@ -1,9 +1,28 @@
+require(ggplot2)
+require(tidyverse)
+convert <- function(x) {
+  training_over_time = as.character(x)
+  training_over_time = gsub("\\[|\\]", "", training_over_time)
+  training_over_time = 1 - (as.numeric(strsplit(training_over_time, ",")[[1]]))
+  abs(training_over_time)
+}
+
+convertShapes <- function(x) {
+  training_over_time = as.character(x)
+  training_over_time = gsub("\\[|\\]", "", training_over_time)
+  training_over_time = as.numeric(strsplit(training_over_time, ",")[[1]])
+  abs(training_over_time)
+}
+
 experiments <- read.csv(file="C:/Users/emmet/Desktop/experiments1-30.csv")
 experiments$trainingAccuracyOverTime <- lapply(experiments$trainingLossOverTime, convert)
 experiments$validationAccuracyOverTime <- lapply(experiments$validationLossOverTime, convert)
-experiments$neuralNetHiddenStructure <- lapply(experiments$neuralNetHiddenStructure, convert)
+experiments$neuralNetHiddenStructure <- lapply(experiments$neuralNetHiddenStructure, convertShapes)
 experiments$inputShape <- as.numeric(experiments$inputShape)
 experiments$outputShape <- as.numeric(experiments$outputShape)
+
+hiddenStructures <- lapply(experiments$neuralNetHiddenStructure, convertShapes)
+hiddenStructuresBroken <- lapply(experiments$neuralNetHiddenStructure, convert)
 
 getRowIndex <- function(shape) {
   rowIndex <- Position(function(x) identical(x, shape), experiments$neuralNetHiddenStructure)
@@ -39,54 +58,66 @@ averageTrainingRate <- function(shape) {
 }
 
 shapeToFinalAccuracy <- function(shape) {
-  length <- length(shapeToAccuracyList(shape))
-  finalAccuracy <- epochToAccuracy(length, shape)
-  return (finalAccuracy)
+  rowIndex <- getRowIndex(shape)
+  validations <- experiments$validationAccuracyOverTime[[rowIndex]]
+  return (validations[length(validations)])
 }
 
-# GGPLOT STUFF
-# GRAPH NAMES:
-# neuronCountToTrainingRate
-# layerCountToTrainingRate
-# weightCountToTrainingRate
-# layerSizeToTrainingRate
-
+# GGPLOT                    
 options(scipen=999)
 library(ggplot2)
 
-# GRAPH TOTAL NUMBER OF NEURONS VS. TRAINING RATE
 allShapes = experiments$neuralNetHiddenStructure
-neuronCounts = as.numeric(lapply(allShapes, sum))
 averageRates = as.numeric(lapply(allShapes, averageTrainingRate)) * 100
 finalRates = as.numeric(lapply(allShapes, shapeToFinalAccuracy)) * 100
+
+
+# GRAPH TOTAL NUMBER OF NEURONS VS. TRAINING RATE
+neuronCounts = as.numeric(lapply(allShapes, sum))
 neuronsRateTable = data.frame(neuronCounts, averageRates)
 neuronCountToTrainingRate <- ggplot(neuronsRateTable, aes(x=neuronCounts, y=averageRates)) + geom_point() + geom_smooth(method="lm") + xlab("Total Neurons") + ylab("Training Rate (%)")
 
 # GRAPH TOTAL NUMBER OF LAYERS VS. TRAINING RATE
 layerCounts = as.numeric(lapply(allShapes, length))
 layersRateTable = data.frame(layerCounts, averageRates)
-layerCountToTrainingRate <- ggplot(layersRateTable, aes(x=layerCounts, y=averageRates)) + geom_point() + geom_smooth(method="lm") + xlab("Total Layers in Structure") + ylab("Training Rate (%)")
+layerCountToTrainingRate <- ggplot(layersRateTable, aes(x=layerCounts, y=averageRates)) + geom_point() + xlab("Total Layers in Structure") + ylab("Training Rate (%)")
 
 # GRAPH WEIGHT COUNT VS. TRAINING RATE
-# NOTE: This only includes HIDDEN WEIGHTS (input and output weights not included...)
+# Function that gives weight count based on shape
 shapeToWeightCount <- function(shape) {
-  accumulator = 2 * shape[1] + shape[length(shape)]
-  for (val in c(1:(length(shape) - 1))) {
-    accumulator = accumulator + (shape[val] * shape[val + 1])
+  shape <- list(shape)
+  if (length(shape[[1]]) == 1) {
+    return (3 * shape[[1]][1])
+  }
+  accumulator = 2 * shape[[1]][1] + shape[[1]][length(shape[[1]])]
+  for (val in c(1:(length(shape[[1]]) - 1))) {
+    accumulator = accumulator + (shape[[1]][val] * shape[[1]][val + 1])
   }
   return(accumulator)
 }
 # weight count vs training rate
-weightCounts = as.numeric(lapply(allShapes, shapeToWeightCount))
+weightCounts <- as.numeric(lapply(allShapes, shapeToWeightCount))
 weightsRateTable = data.frame(weightCounts, averageRates)
 weightCountToTrainingRate <- ggplot(weightsRateTable, aes(x=weightCounts, y=averageRates)) + geom_point() + geom_smooth(method="lm") + xlab("Total Weights in Structure") + ylab("Training Rate (%)")
 # weight count vs. final accuracy
 weightsFinalTable = data.frame(weightCounts, finalRates)
-# this graph has 3 very odd distinct bands - cant explain this at the moment
+# TRIPLE BAND GRAPH!!!!!!!!!!!!! compares total number of weghts to the final accuracy of the structure
 weightCountToFinalRate <- ggplot(weightsFinalTable, aes(x=weightCounts, y=finalRates)) + geom_point() + xlab("Total Weights in Structure") + ylab("FinalAccuracy (%)")
 
 # GRAPH AVERAGE LAYER SIZE VS. TRAINING RATE
-averageLayerSize = as.numeric(lapply(allShapes, mean))
-layerSizeRateTable = data.frame(averageLayerSize, averageRates)
-layerSizeToTrainingRate <- ggplot(layerSizeRateTable, aes(x=averageLayerSize, y=averageRates)) + geom_point() + geom_smooth(method="lm") + xlab("Average Layer Size") + ylab("Training Rate (%)")
+# averageLayerSize = as.numeric(lapply(allShapes, mean))
+# layerSizeRateTable = data.frame(averageLayerSize, averageRates)
+# layerSizeToTrainingRate <- ggplot(layerSizeRateTable, aes(x=averageLayerSize, y=averageRates)) + geom_point() + geom_smooth(method="lm") + xlab("Average Layer Size") + ylab("Training Rate (%)")
 
+
+# MAKE TABLES OF TOP 10 and WORST 10 STRUCTURES
+library(plyr)
+unlistedShapes <- lapply(allShapes, unlist)
+shapesTable <- data.frame(allShapes, finalRates, ncol = 2)
+shapesRatesTable <- do.call(rbind.fill, lapply(lapply(lapply(unlistedShapes, data.frame), t), data.frame))
+shapesRatesTable <- cbind(shapesRatesTable, averageRates, finalRates)
+
+order.training <- order(shapesRatesTable$averageRates, decreasing = TRUE)
+shapesRatesbyTraining <- shapesRatesTable[order.training,]
+order.final <- order(shapesRatesTable$finalRates, decreasing = TRUE)
+shapesRatesbyFinal <- shapesRatesTable[order.final, ]
